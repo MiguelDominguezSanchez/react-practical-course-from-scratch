bf33b513725da68f6a0941318452e3f8088b6123
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-10 10:19:12 +0200

    Hooks(useEffect, useState) - Mapping an arrry with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import * as API from './services/launches.js'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+	const [launches, setLaunches] = useState([])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	useEffect(() => {
+		API.getAllLaunches().then(setLaunches)
+	}, [])
+
+	return (
+		<>
+			<h1>SpaceX Launches</h1>
+			<ul>
+				{launches.map((launch) => (
+					<li key={launch.flight_number}>
+						{launch.mission_name}({launch.length_year})
+					</li>
+				))}
+			</ul>
+		</>
+	)
 }
-
-export default App
